d4215e78d33b97c6dbe52e40a2e1368b37b5fe0a
io23632 <io23632@bristol.ac.uk>
2024-02-09 19:16:52 +0000

    nonesence commit

--- Easy/EasyProblems/src/main/java/hamza/leet/TwoSum/TwoSum.java
+++ Easy/EasyProblems/src/main/java/hamza/leet/TwoSum/TwoSum.java
@@ -4,22 +4,22 @@ public class TwoSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of elements:");
-
         // Read the number of elements to be stored in the array
         int n = scanner.nextInt();
-
         // Declare an array to store the numbers
         int[] nums = new int[n];
-
         System.out.println("Enter " + n + " numbers:");
-
         // Loop to read each number into the array
         for (int i = 0; i < n; i++) {
             nums[i] = scanner.nextInt();
         }
+        // Enter your target
         System.out.println("Enter your target: ");
         int target = scanner.nextInt();
-        System.out.println("Your target is :" + target);
+
+
+
+
 
 
 
