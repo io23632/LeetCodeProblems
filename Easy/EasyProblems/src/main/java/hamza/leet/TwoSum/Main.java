63a633502b893a5ddb4dcf93e60f394c4389e61a
io23632 <io23632@bristol.ac.uk>
2024-02-09 19:13:38 +0000

    [twosum:] added printline your target is:

--- Easy/EasyProblems/src/main/java/hamza/leet/TwoSum/Main.java
+++
@@ -1,5 +0,0 @@
-package hamza.leet.TwoSum;
-
-public class Main {
-
-}
---
+++ Easy/EasyProblems/src/main/java/hamza/leet/TwoSum/TwoSum.java
@@ -0,0 +1,29 @@
+package hamza.leet.TwoSum;
+import java.util.Scanner;
+public class TwoSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements:");
+
+        // Read the number of elements to be stored in the array
+        int n = scanner.nextInt();
+
+        // Declare an array to store the numbers
+        int[] nums = new int[n];
+
+        System.out.println("Enter " + n + " numbers:");
+
+        // Loop to read each number into the array
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        System.out.println("Enter your target: ");
+        int target = scanner.nextInt();
+        System.out.println("Your target is :" + target);
+
+
+
+
+        scanner.close(); // Close the scanner to prevent resource leak
+    }
+}
