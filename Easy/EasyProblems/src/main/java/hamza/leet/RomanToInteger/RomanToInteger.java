ebecfaac0916c7de3fa474abd30fa8a24ab31120
io23632 <io23632@bristol.ac.uk>
2024-02-11 13:26:40 +0000

    removed spaces

--- Easy/EasyProblems/src/main/java/hamza/leet/RomanToInteger/RomanToInteger.java
+++ Easy/EasyProblems/src/main/java/hamza/leet/RomanToInteger/RomanToInteger.java
@@ -3,9 +3,7 @@ package hamza.leet.RomanToInteger;
 public class RomanToInteger {
 
     public static void main(String[] args) {
-
         Solution1 solution = new Solution1();
-
         int number = solution.romanToInt("MCMXCIV");
         System.out.println(number);
 
